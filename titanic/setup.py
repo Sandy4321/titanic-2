b358fb90c5bab27b100bdededf30d6dbd28141ab
Jonathan Miller <AFTRTCH@gmail.com>
2018-01-17 11:56:51 -0500

    Add data pipeline and command line tool to launch it

--- titanic/setup.py
+++ titanic/setup.py
@@ -29,7 +29,12 @@ setup(
         'pyyaml>=3.12',
         'pytest>=3.2.3',
         'pytest-runner>=2.12.1',
+        'click>=6.7',
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
+    entry_points='''
+        [console_scripts]
+        titanic_analysis=titanic.command_line:titanic_analysis
+    '''
 )
